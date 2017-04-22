8e75d0fc2024042d445d0e65e773ec8a4744e546
JordanPag <jpags2938@gmail.com>
2017-04-21 20:22:16 -0400

    INFINITE PAGES

--- sinatra.rb
+++ sinatra.rb
@@ -4,10 +4,6 @@ get '/' do
   'SINATRA IS BEAST!!!!!!!!!!!!!!!!!!!!!!!!!'
 end
 
-get '/bold' do
-  'SINATRA IS <b>BEAST!!!!!!!!!!!!!!!!!!!!!!!!!</b>'
-end
-
-get '/italics' do
-  'SINATRA IS <i>BEAST!!!!!!!!!!!!!!!!!!!!!!!!!</i>'
+get '/:id' do
+	"#{params[:id]}"
 end
