8a94fbc9c06931bf4a6618ae7b167fa2be4de11e
JordanPag <jpags2938@gmail.com>
2017-04-21 20:14:33 -0400

    Change

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
 get '/' do
+  'SINATRA IS BEAST!!!!!!!!!!!!!!!!!!!!!!!!!'
+end
+
+get '/bold' do
   'SINATRA IS <b>BEAST!!!!!!!!!!!!!!!!!!!!!!!!!</b>'
+end
+
+get '/italics' do
+  'SINATRA IS <i>BEAST!!!!!!!!!!!!!!!!!!!!!!!!!</i>'
 end
