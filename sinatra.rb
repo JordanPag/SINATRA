0a510ed98c61fa3a5b110fb2871ea2055b95af64
JordanPag <jpags2938@gmail.com>
2017-04-21 20:35:21 -0400

    Commit

--- sinatra.rb
+++ sinatra.rb
@@ -6,4 +6,14 @@ end
 
 get '/:id' do
 	"#{params[:id]}"
+	desc "This task is called by the Heroku scheduler add-on"
+	task :update_feed => :environment do
+	  puts "Updating feed..."
+	  NewsFeed.update
+	  puts "done."
+	end
+
+	task :send_reminders => :environment do
+	  User.send_reminders
+	end
 end
